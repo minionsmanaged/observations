793159f1c7a72c989f5d1f5cfb990809961b2270
Rob Thijssen <rthijssen@gmail.com>
2020-04-04 13:54:06 +0300

    list instance file contents

--- ci/observe-tasks.py
+++ ci/observe-tasks.py
@@ -10,4 +10,4 @@ print(pool)
 for instance_file_path in glob.glob('workers/{}/*.json'.format(pool)):
   with open(instance_file_path) as instance_file:
     instance = json.load(instance_file)
-    print('{}/{} with {} in {} was launched at {}'.format(instance.WorkerPool, instance.InstanceId, instance.ImageId, instance.AvailabilityZone, instance.LaunchTime))
+    print('{}/{} with {} in {} was launched at {}'.format(instance['WorkerPool'], instance['InstanceId'], instance['ImageId'], instance['AvailabilityZone'], instance['LaunchTime']))
