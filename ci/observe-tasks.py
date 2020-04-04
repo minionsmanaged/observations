693e980e3549bd3c234d57664e736bd64532dddc
Rob Thijssen <rthijssen@gmail.com>
2020-04-04 13:39:58 +0300

    list instance files

--- ci/observe-tasks.py
+++ ci/observe-tasks.py
@@ -1,3 +1,13 @@
+import glob
 import os
 
-print(os.environ['pool'])
+
+pool = os.environ['pool']
+print(pool)
+
+
+for instance_file in glob.glob('workers/{}/*.json'.format(pool)):
+  print(instance_file)
+
+  #with open('workers/{}') as f:
+  #  data = json.load(f)
