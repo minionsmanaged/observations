4dea2495c7ce52c23399c893c8561cb54b2df47c
Rob Thijssen <rthijssen@gmail.com>
2020-04-04 15:04:55 +0300

    push task data

---
+++ ci/push-task-observations.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+random_delay=$((1 + RANDOM % 60))
+echo waiting ${random_delay} seconds
+sleep ${random_delay}
+git pull
+git add workers/${pool}
+git commit -m "tc queue observations for ${pool}"
+git push --quiet "https://${GH_TOKEN}@github.com/minionsmanaged/observations.git" master:master
