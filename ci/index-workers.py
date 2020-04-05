dfe3594b121be9931c507f42f427d5ce9e1a453b
Rob Thijssen <rthijssen@gmail.com>
2020-04-05 08:50:32 +0300

    pool counts

--- ci/index-workers.py
+++ ci/index-workers.py
@@ -7,16 +7,16 @@ pools = {}
 for instance_file_path in glob.glob('workers/**/*.json', recursive = True):
   with open(instance_file_path, 'r') as instance_file_read:
     instance = json.load(instance_file_read)
-    domain, worker = instance['WorkerPool'].split('/')
+    domain, pool = instance['WorkerPool'].split('/')
     project = domain[:domain.rindex('-')] if '-' in domain else domain
     if not project in pools:
       pools[project] = {}
     if not domain in pools[project]:
       pools[project][domain] = {}
-    if not worker in pools[project][domain]:
-      pools[project][domain][worker] = 1
+    if not pool in pools[project][domain]:
+      pools[project][domain][pool] = 1
     else:
-      pools[project][domain][worker] += 1
+      pools[project][domain][pool] += 1
     if not instance['WorkerPool'] in workers:
       workers[instance['WorkerPool']] = []
     worker = {
@@ -31,6 +31,7 @@ for instance_file_path in glob.glob('workers/**/*.json', recursive = True):
       if 'recentTasks' in instance['tc']:
         worker['tasks'] = list(map(lambda x: '{}/{}'.format(x['taskId'], x['runId']), instance['tc']['recentTasks']))
     workers[instance['WorkerPool']].append(worker)
+    print('{}/{}/{}/{} indexed'.format(project, domain, worker, project))
 
 pools_index_path = 'pools.json'
 try:
@@ -41,11 +42,11 @@ with open(pools_index_path, 'w') as pools_file:
   json.dump(pools, pools_file, indent = 2)
 for project in pools:
   for domain in project:
-    for worker in domain:
-      pool_index_path = '{}-{}.json'.format(domain, worker)
+    for pool in domain:
+      pool_index_path = '{}-{}.json'.format(domain, pool)
       try:
         os.remove(pool_index_path)
       except OSError:
         pass
       with open(pool_index_path, 'w') as pool_file:
-        json.dump(workers['{}/{}'.format(domain, worker)], pool_file, indent = 2)
+        json.dump(workers['{}/{}'.format(domain, pool)], pool_file, indent = 2)
