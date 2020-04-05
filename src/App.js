6049a8370e2eb5925a422c53e29015141abe7d73
Rob Thijssen <rthijssen@gmail.com>
2020-04-05 10:45:18 +0300

    scaffold pool render

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      pools: {}
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
+  
+  componentDidMount() {
+    this.fetchData();
+  }
+  
+  fetchData() {
+    fetch('https://raw.githubusercontent.com/minionsmanaged/observations/master/pools.json')
+      .then(response => response.json())
+      .then(pools => {
+        this.setState(state => {
+          state.pools = pools;
+          return state;
+        });
+      });
+  }
+
+  render() {
+    return (
+      <Container>
+        {
+          Object.keys(this.state.pools).filter(x => x !== 'count').sort().map(project => (
+            <Row key={project}>
+              <div>
+                <h3>{project}</h3>
+                {
+                  Object.keys(this.state.pools[project]).filter(x => x !== 'count').sort().map(domain => (
+                    <div key={domain}>
+                      <h4>{domain}</h4>
+                      {
+                        Object.keys(this.state.pools[project][domain]).filter(x => x !== 'count').sort().map(pool => (
+                          <div key={pool}>
+                            <h5>{pool}</h5>
+                          </div>
+                        ))
+                      }
+                    </div>
+                  ))
+                }
+              </div>
+              <hr />
+            </Row>
+          ))
+        }
+      </Container>
+    );
+  }
 }
 
 export default App;
