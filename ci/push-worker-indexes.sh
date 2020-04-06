0a21565f69769ccb9ec6f1c9fe9860e997afce8a
Rob Thijssen <rthijssen@gmail.com>
2020-04-06 13:34:55 +0300

    clean up push

--- ci/push-worker-indexes.sh
+++ ci/push-worker-indexes.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-${script_dir}/attempt-push.sh "./*.json" "worker index"
+git config user.name "${GIT_NAME}"
+git config user.email "${GIT_EMAIL}"
+
+random_uuid=$(uuidgen --random)
+temp_branch=${random_uuid:(-12)}
+git checkout -b ${temp_branch}
+git add ./*.json
+git commit -m "worker index"
+git format-patch master --stdout > ${temp_branch}.patch
+git checkout master
+git pull
+if git apply --stat ${temp_branch}.patch && git apply --check ${temp_branch}.patch; then
+  git apply ${temp_branch}.patch
+  git push --quiet "https://${GH_TOKEN}@github.com/minionsmanaged/observations.git" master:master
+fi
