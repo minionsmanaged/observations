2e13b9deaade5a6c7b80eae5453267ec1b62b9d9
Rob Thijssen <rthijssen@gmail.com>
2020-04-06 15:35:15 +0300

    store tasks in own folder

--- ci/observe-tasks.py
+++ ci/observe-tasks.py
@@ -19,12 +19,18 @@ for instance_file_path in glob.glob('workers/{}/*.json'.format(pool)):
     instance_queue_url = 'https://firefox-ci-tc.services.mozilla.com/api/queue/v1/provisioners/{}/worker-types/{}/workers/aws/{}'.format(workerDomain, workerType, instance['InstanceId'])
     try:
       with urllib.request.urlopen(instance_queue_url) as response:
-        instance_queue_data = json.loads(response.read().decode())
-        if instance_queue_data is not None:
-          if 'recentTasks' in instance_queue_data and len(instance_queue_data['recentTasks']) > 0:
-            instance_tasks = map(lambda x: { 'task': x['taskId'], 'run': x['runId'] }, instance_queue_data['recentTasks'])
-            instance_tasks_file_path = instance_file_path.replace('/workers/', '/tasks/')
-            with open(instance_tasks_file_path, 'w') as instance_tasks_file_write:
-              json.dump({ 'claim': instance_queue_data['firstClaim'], 'expires': instance_queue_data['expires'], 'tasks': instance_tasks }, instance_tasks_file_write, indent = 2)
+        queue_response = json.loads(response.read().decode())
+        if queue_response is not None:
+          instance_queue_data = {}
+          if 'firstClaim' in queue_response:
+            instance_queue_data['claim'] = queue_response['firstClaim']
+          if 'expires' in queue_response:
+            instance_queue_data['expires'] = queue_response['expires']
+          if 'recentTasks' in queue_response and len(queue_response['recentTasks']) > 0:
+            instance_queue_data['tasks'] = map(lambda x: { 'task': x['taskId'], 'run': x['runId'] }, queue_response['recentTasks'])
+
+          instance_tasks_file_path = instance_file_path.replace('/workers/', '/tasks/')
+          with open(instance_tasks_file_path, 'w') as instance_tasks_file_write:
+            json.dump(instance_queue_data, instance_tasks_file_write, indent = 2)
     except urllib.error.HTTPError as err:
       print('error reading: {}'.format(instance_queue_url))
