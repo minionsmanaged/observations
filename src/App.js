48b3819f3dcac6aad641c5d413cf80dffdfe0137
Rob Thijssen <rthijssen@gmail.com>
2020-04-06 20:07:20 +0300

    wip: filters

--- src/App.js
+++ src/App.js
@@ -5,37 +5,48 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
 import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Form from 'react-bootstrap/Form';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      projects: [
-        'gecko'
-      ],
-      domains: [
-        'gecko-1',
-        'gecko-3',
-        'gecko-t'
-      ],
-      workers: [
-        'b-linux',
-        'b-linux-aws',
-        'b-linux-xlarge',
-        'b-win2012',
-        't-win10-64',
-        't-win10-64-gpu-s',
-        't-win7-32',
-        't-win7-32-gpu',
-        't-linux-large',
-        't-linux-xlarge'
-      ],
+      filter: {
+        option: {
+          projects: [],
+          domains: [],
+          workers: []
+        },
+        selection: {
+          projects: [
+            'gecko'
+          ],
+          domains: [
+            'gecko-1',
+            'gecko-3',
+            'gecko-t'
+          ],
+          workers: [
+            'b-linux',
+            'b-linux-aws',
+            'b-linux-xlarge',
+            'b-win2012',
+            't-win10-64',
+            't-win10-64-gpu-s',
+            't-win7-32',
+            't-win7-32-gpu',
+            't-linux-large',
+            't-linux-xlarge'
+          ]
+        }
+      },
       columns: 4,
       fluid: false,
       pools: {}
     };
     this.fetchData = this.fetchData.bind(this);
+    //this.handleFilterChange = this.handleFilterChange.bind(this);
   }
   
   componentDidMount() {
@@ -65,22 +76,68 @@ class App extends React.Component {
       .then(response => response.json())
       .then(pools => {
         this.setState(state => {
-          // filter by state.projects
-          state.pools = (state.projects === undefined || state.projects.length === 0)
+          // filter by state.filter.selection.projects
+          state.pools = (state.filter.selection.projects === undefined || state.filter.selection.projects.length === 0)
             ? pools
-            : state.projects.reduce((o, k) => ({ ...o, [k]: pools[k] }), {});
+            : state.filter.selection.projects.reduce((o, k) => ({ ...o, [k]: pools[k] }), {});
+
+          // populate filter options
+          state.filter.option.projects = Object.keys(pools).filter(x => x !== 'count').sort();
+          state.filter.option.projects.forEach(project => {
+            state.filter.option.domains = [...new Set([...state.filter.option.domains, ...Object.keys(pools[project]).filter(x => x !== 'count')])].sort();
+            Object.keys(pools[project]).forEach(domain => {
+              state.filter.option.workers = [...new Set([...state.filter.option.workers, ...Object.keys(pools[project][domain]).filter(x => x !== 'count')])].sort();
+            });
+          });
           return state;
         });
       });
   }
 
+  /*
+  handleFilterChange(event) {
+    let id = event.target.id.split('_');
+    let filter = id[1];
+    let property = id[2];
+    this.setState(state => {
+      let index = state.filter.selection[filter].indexOf(property);
+      if (index > -1) {
+        state.filter.selection[filter].splice(index, 1);
+      } else {
+        state.filter.selection[filter].push(property);
+      }
+      return state;
+    });
+  }
+  */
+
   render() {
     return (
       <Container fluid={this.state.fluid}>
         <Row>
+          <Col>
+            {
+              ['projects', 'domains', 'workers'].map(filter => (
+                <div>
+                  <h6>{filter}</h6>
+                  {
+                    this.state.filter.option[filter].map(property => (
+                      <Form.Check
+                        type="checkbox"
+                        onChange={()=>{}/*this.handleFilterChange*/}
+                        checked={this.state.filter.selection[filter].includes(property)}
+                        label={property}
+                        id={'filter_' + filter + '_' + property}
+                        key={'filter_' + filter + '_' + property} />
+                    ))
+                  }
+                </div>
+              ))
+            }
+          </Col>
         {
           [...Array(this.state.columns).keys()].map(column =>(
-            <Col key={column}>
+            <Col key={column} xs={10}>
               {
                 Object.keys(this.state.pools).sort((pA, pB) => { return ((Object.keys(this.state.pools[pA]).length) < (Object.keys(this.state.pools[pB]).length)) ? -1 : ((Object.keys(this.state.pools[pA]).length) > (Object.keys(this.state.pools[pB]).length)) ? 1 : 0 }).filter((x, i) => ((i % this.state.columns === column) && (x !== 'count'))).map(project => (
                   <Card key={project}>
